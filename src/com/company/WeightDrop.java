024306cc9e3a9c623d0b30a1c81c3b9f954a9ba4
takred <sazonov.kos@yandex.ru>
2018-12-20 00:50:16 +0300

    Добавлен метод collect в клссс WeightDrop.

--- src/com/company/WeightDrop.java
+++ src/com/company/WeightDrop.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class WeightDrop {
     String name;
@@ -27,8 +28,40 @@ public class WeightDrop {
     }
 
     static List<Item> collect (List<Item> inventHero, List<Item> allItems, List<WeightDrop> monsterItemDrops){
-        List<Item> result = new ArrayList<>();
-        return result;
+        if (monsterItemDrops.size() > 0) {
+        int drop = ThreadLocalRandom.current().nextInt(1, 100);
+        if (drop <= 10) {
+            int dropItem;
+            if (monsterItemDrops.size() > 1) {
+                int sumWeight = 0;
+                for (int i = 0; i < monsterItemDrops.size(); i++){
+                    sumWeight = sumWeight + monsterItemDrops.get(i).weight;
+                }
+                dropItem = ThreadLocalRandom.current().nextInt(1, sumWeight);
+            }
+            else{
+                dropItem = 0;
+            }
+            if (monsterItemDrops.size() > 0) {
+                int weightCurrent = 0;
+                for (int i = 0; i < monsterItemDrops.size(); i++) {
+                    weightCurrent = weightCurrent + monsterItemDrops.get(i).weight;
+                    if (dropItem <= weightCurrent) {
+                        String itemName = monsterItemDrops.get(i).name;
+                        for (int j = 0; j < allItems.size(); j++) {
+                            if (itemName.equals(allItems.get(j).name)) {
+                                inventHero.add(allItems.get(j));
+                                System.out.println("Вам выпал предмет \"" + itemName + "\".");
+                                return inventHero;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+        System.out.println("Ошибка! Этого не должно было случиться.");
+        return inventHero;
 
     }
 }
