e32f2a9b99c49eb4cfb4eeadee4afe6f88ea7e15
takred <sazonov.kos@yandex.ru>
2019-03-15 11:41:09 +0300

    Старая сказка на новый лад или "Деградация во благо!"

---
+++ src/InventoryDollCopy.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class InventoryDollCopy {
+    String nameHero;
+    Item head;
+    Item torso;
+    Item legs;
+    Item weapon;
+
+    public InventoryDollCopy() {
+        head = null;
+        torso = null;
+        legs = null;
+        weapon = null;
+    }
+
+    public List<Item> items() {
+        List<Item> equippedItem = new ArrayList<>();
+        equippedItem.add(head);
+        equippedItem.add(torso);
+        equippedItem.add(legs);
+        equippedItem.add(weapon);
+        return Collections.unmodifiableList(equippedItem); }
+
+    public void putOn(Item item) {
+        if (item.category == 0){
+            head = item;
+        }
+        else if (item.category == 1){
+            torso = item;
+            } else if (item.category == 1){
+            legs = item;
+            } else if (item.category == 1){
+            weapon = item;
+            }
+//        equippedItem.set(item.category, item);
+    }
+
+    public Item takeOff(int category) {
+        Item item = null;
+        if (category == 0){
+            item = head;
+            head = null;
+        }
+        else if (category == 1){
+            item = torso;
+            torso = null;
+        }else if (category == 2){
+            item = legs;
+            legs = null;
+        }else if (category == 3){
+            item = weapon;
+            weapon = null;
+        }
+        return item;
+    }
+
+    public boolean isOn(int category) {
+        Item item = null;
+        if (category == 0){
+            item = head;
+        }else if (category == 1){
+            item = torso;
+        }else if (category == 2){
+            item = legs;
+        }else if (category == 3){
+            item = weapon;
+        }
+        return item == null;
+    }
+}
