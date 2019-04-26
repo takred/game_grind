c8260e5c82744aee1af7521c324dd3c143c2a29f
takred <sazonov.kos@yandex.ru>
2019-04-26 12:38:26 +0300

    Новая ветка.

--- src/com/company/PersistentInventory.java
+++ src/com/company/PersistentInventory.java
@@ -5,28 +5,31 @@ import com.company.items.Item;
 import java.util.List;
 
 public class PersistentInventory implements GrindInventory {
+
+    private GrindInventory grindInventory;
+
     @Override
     public String nameHero() {
-        return null;
+        return grindInventory.nameHero();
     }
 
     @Override
     public void nameHeroAdd(String name) {
-
+        grindInventory.nameHeroAdd(name);
     }
 
     @Override
     public List<Item> items() {
-        return null;
+        return grindInventory.items();
     }
 
     @Override
     public void add(Item item) {
-
+        grindInventory.add(item);
     }
 
     @Override
     public Item take(int slot) {
-        return null;
+        return grindInventory.take(slot);
     }
 }
