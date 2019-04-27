c65cd51a335eb8812555002d924a2b73028506c8
takred <sazonov.kos@yandex.ru>
2019-04-27 20:40:19 +0300

    Реструктуризация под maven.

--- src/Equip.java
+++
@@ -1,8 +0,0 @@
-import com.company.items.Item;
-
-import java.util.List;
-
-public class Equip {
-    List<Item> invent;
-    List<Item> equipInvent;
-}
--- src/main/java/com/company/FileInventory.java
+++ src/main/java/com/company/FileInventory.java
@@ -25,19 +25,19 @@ public class FileInventory implements GrindInventory {
         for (int i = 1; i < strings.size(); i++){
             String iElement = strings.get(i);
             int category = Integer.parseInt(iElement.substring(0, 1));
-            int startIndName = iElement.indexOf("\"");
-            int endIndName = iElement.indexOf("\"", startIndName + 1);
-            int endIndMinStr = iElement.indexOf(",", endIndName + 2);
+            int startIdxName = iElement.indexOf("\"");
+            int endIdxName = iElement.indexOf("\"", startIdxName + 1);
+            int endIdxMinStr = iElement.indexOf(",", endIdxName + 2);
             Item item;
             if (category != 3){
-                String name = iElement.substring(startIndName + 1, endIndName);
-                int plusMaxHp = Integer.parseInt(iElement.substring(endIndName + 2));
+                String name = iElement.substring(startIdxName + 1, endIdxName);
+                int plusMaxHp = Integer.parseInt(iElement.substring(endIdxName + 2));
                 item = new Item(name, plusMaxHp, category);
                 heroInv.add(item);
             } else {
-                String name = iElement.substring(startIndName + 1, endIndName);
-                int plusMinStr = Integer.parseInt(iElement.substring(endIndName + 2, endIndMinStr));
-                int plusMaxStr = Integer.parseInt(iElement.substring(endIndMinStr + 1));
+                String name = iElement.substring(startIdxName + 1, endIdxName);
+                int plusMinStr = Integer.parseInt(iElement.substring(endIdxName + 2, endIdxMinStr));
+                int plusMaxStr = Integer.parseInt(iElement.substring(endIdxMinStr + 1));
                 item = new Item(name, plusMinStr, plusMaxStr, category);
                 heroInv.add(item);
             }
